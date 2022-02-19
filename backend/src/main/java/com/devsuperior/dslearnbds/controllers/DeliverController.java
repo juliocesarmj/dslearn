96dc660e7f872e4c9d586f036c5c56dfb1629238
Julio Moraes <juliocesardemoraes2015@gmail.com>
2022-02-19 17:29:32 -0300

    PreAuthorizing methods

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/DeliverController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dslearnbds.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverController {
+	
+	@Autowired
+	private DeliverService service;
+	
+	@PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id ,@RequestBody DeliverRevisionDTO dto) {
+		this.service.saveRevision(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/DeliverRevisionDTO.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dslearnbds.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dslearnbds.entities.enums.DeliverStatus;
+
+public class DeliverRevisionDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private DeliverStatus status;
+	private String feedback;
+	private Integer correctCount;
+	
+	public DeliverStatus getStatus() {
+		return status;
+	}
+	public void setStatus(DeliverStatus status) {
+		this.status = status;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	public Integer getCorrectCount() {
+		return correctCount;
+	}
+	public void setCorrectCount(Integer correctCount) {
+		this.correctCount = correctCount;
+	}
+	
+	
+}
